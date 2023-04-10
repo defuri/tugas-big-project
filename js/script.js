13cbaee02d2d8c737f683f3f0a0f3a8c9a4e08d0
Rexynama <ghazwanfikri524@gmail.com>
2023-04-10 10:17:56 +0700

    animasi nav

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 window.addEventListener("scroll", function() {
     var navbar = document.querySelector(".kontainer-nav");
     navbar.classList.toggle("scrolled", window.scrollY > 50);
+    var navbarhilang = document.querySelector(".kontainer-nav");
+    navbarhilang.classList.toggle("scrolled2", window.scrollY > 1200);
+});
+window.addEventListener("scroll", function() {
+    var navbar1 = document.querySelector("#navigasi");
+    navbar1.classList.toggle("scrolled1", window.scrollY > 50);
 });
