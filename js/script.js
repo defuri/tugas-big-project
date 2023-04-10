f40dc4bf3a4b2073a502ea60373e5eaa25f4b54e
Rexynama <ghazwanfikri524@gmail.com>
2023-04-10 10:30:42 +0700

    edit java

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function() {
     var navbar = document.querySelector(".kontainer-nav");
     navbar.classList.toggle("scrolled", window.scrollY > 50);
     var navbarhilang = document.querySelector(".kontainer-nav");
-    navbarhilang.classList.toggle("scrolled2", window.scrollY > 1200);
+    navbarhilang.classList.toggle("scrolled2", window.scrollY > 1256);
 });
 window.addEventListener("scroll", function() {
     var navbar1 = document.querySelector("#navigasi");
