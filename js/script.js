5e065be6b1bc0091f91a2b3219ccf7fd312126f1
mdefriansyah <endangaja762@gmail.com>
2023-05-02 20:16:16 +0700

    backup

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     var navbar = document.querySelector(".kontainer-nav");
     navbar.classList.toggle("scrolled", window.scrollY > 50);
     var navbar1 = document.querySelector("#navigasi");
@@ -7,6 +7,7 @@ window.addEventListener("scroll", function() {
     navbarhilang.classList.toggle("scrolled2", window.scrollY > 1256);
 });
 
+<<<<<<< Updated upstream
 const wrapper = document.querySelector(".wrapper");
 const links = document.querySelectorAll(".link");
 const btnPopup = document.querySelectorAll(".btn-popup");
@@ -24,3 +25,41 @@ btnPopup.forEach(link => {
         wrapper.classList.toggle("active-btn")
     });
 });
+=======
+let login = document.getElementById('login-popup');
+let bg = document.getElementById('login-background');
+
+function openLogin() {
+    login.classList.add('active-btn');
+    bg.classList.add('active-btn');
+}
+
+function closeLogin() {
+    login.classList.remove('active-btn');
+    bg.classList.remove('active-btn');
+}
+
+function switchRegister() {
+    login.classList.add('active');
+}
+
+function switchLogin() {
+    login.classList.remove('active');
+}
+
+let bool = true; //!boolean untuk fungsi njir
+
+
+function muncul() {
+    const aidi = document.getElementById("isi");
+    let cetrekan = true;
+
+    if(cetrekan == true) {
+        aidi.style.display = "block";
+        cetrekan = false;
+    } else {
+        aidi.style.display = "none";
+        cetrekan = true;
+    }
+}
+>>>>>>> Stashed changes
