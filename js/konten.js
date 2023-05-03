327a42007a99fb9142ddce74469de7e5e9b9f204
mdefriansyah <endangaja762@gmail.com>
2023-05-03 17:51:12 +0700

    nambah daftar isi

---
+++ js/konten.js
@@ -0,0 +1,41 @@
+function munculIlang() {
+    let aidi = document.getElementById("listIlang");
+    let aidi2 = document.getElementById("memulai");
+
+    if(aidi.style.display == "none" ) {
+
+        aidi.style.display = "block";
+        aidi2.style.listStyle = "url(../images/segitigaBawah.jpeg)";
+    } else {
+        aidi.style.display = "none";
+        aidi2.style.listStyle = "url(../images/segitigaKanan.jpeg)";
+    }
+}
+
+function munculIlang2() {
+    let aidi = document.getElementById("listIlang2");
+    let aidi2 = document.getElementById("memulai2");
+
+    if(aidi.style.display == "none" ) {
+
+        aidi.style.display = "block";
+        aidi2.style.listStyle = "url(../images/segitigaBawah.jpeg)";
+    } else {
+        aidi.style.display = "none";
+        aidi2.style.listStyle = "url(../images/segitigaKanan.jpeg)";
+    }
+}
+
+function munculIlang3() {
+    let aidi = document.getElementById("listIlang3");
+    let aidi2 = document.getElementById("memulai3");
+
+    if(aidi.style.display == "none" ) {
+
+        aidi.style.display = "block";
+        aidi2.style.listStyle = "url(../images/segitigaBawah.jpeg)";
+    } else {
+        aidi.style.display = "none";
+        aidi2.style.listStyle = "url(../images/segitigaKanan.jpeg)";
+    }
+}
