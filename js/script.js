f446d3b7a55fd61bec91ea2f7712b4db721df119
Rexynama <ghazwanfikri524@gmail.com>
2023-05-04 11:29:57 +0700

    Asinkron

--- js/script.js
+++ js/script.js
@@ -170,3 +170,17 @@ function munculIlang10() {
     aidi2.style.listStyle = "url(../images/segitigaKanan.jpeg)";
   }
 }
+
+function munculIlang12() {
+  let aidi = document.getElementById("listIlang12");
+  let aidi2 = document.getElementById("memulai12");
+
+  if (aidi.style.display == "none") {
+
+    aidi.style.display = "block";
+    aidi2.style.listStyle = "url(../images/segitigaBawah.jpeg)";
+  } else {
+    aidi.style.display = "none";
+    aidi2.style.listStyle = "url(../images/segitigaKanan.jpeg)";
+  }
+}
