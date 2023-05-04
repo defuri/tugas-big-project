15203758a37f1fd4cd274a4f31681b0a7c1127b6
mdefriansyah <endangaja762@gmail.com>
2023-05-04 10:12:22 +0700

    a

--- js/konten.js
+++ js/konten.js
@@ -39,3 +39,17 @@ function munculIlang3() {
         aidi2.style.listStyle = "url(../images/segitigaKanan.jpeg)";
     }
 }
+
+function munculIlang4() {
+    let aidi = document.getElementById("listIlang4");
+    let aidi2 = document.getElementById("memulai4");
+
+    if(aidi.style.display == "none" ) {
+
+        aidi.style.display = "block";
+        aidi2.style.listStyle = "url(../images/segitigaBawah.jpeg)";
+    } else {
+        aidi.style.display = "none";
+        aidi2.style.listStyle = "url(../images/segitigaKanan.jpeg)";
+    }
+}
