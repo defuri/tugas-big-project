60669df0979bd6dc85d4d81481ed57e39cdbc024
Rexynama <ghazwanfikri524@gmail.com>
2023-05-07 19:43:32 +0700

    rubah var menjadi let

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 window.addEventListener("scroll", function () {
-  var navbar = document.querySelector(".kontainer-nav");
+  let navbar = document.querySelector(".kontainer-nav");
   navbar.classList.toggle("scrolled", window.scrollY > 50);
-  var navbar1 = document.querySelector("#navigasi");
+  let navbar1 = document.querySelector("#navigasi");
   navbar1.classList.toggle("scrolled1", window.scrollY > 50);
-  var navbarhilang = document.querySelector(".kontainer-nav");
+  let navbarhilang = document.querySelector(".kontainer-nav");
   navbarhilang.classList.toggle("scrolled2", window.scrollY > 1256);
 });
 
